191d8bf5640770fa3e07078bba5e3103f84cc798
nandani2611 <nandanidalsaniya@gmail.com>
2021-04-04 19:39:38 +0530

    changed code added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
+function compute() 
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        document.getElementById("principal").focus();
+        alert("Enter a positive number");
+        return false;
+    }
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("numberr").value;
+    interest = principal * years * rate / 100
+    var d = new Date();
+    var n = d.getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "%</mark>.<br> You will receive an amount of <mark>" + interest + "</mark>,<br> in the year <mark>" + n + "</mark>";
 }
-        
+function show_value(x) 
+{
+    document.getElementById("slider_value").innerHTML = x;
+}
